e4f0fe0da73027aabe17e29fed9f2fce9f1029a0
Jonne Kiukas <jonne.kiukas@hotmail.com>
2019-11-01 12:36:22 +0200

    Calculator task done.

---
+++ HelloWorld/src/app/calculator/calculator.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  buttons: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '0', '+', '='];
+  result: string = '';
+  private prevValue: string = '';
+  private curValue: string = '';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  addValue(value: string) {
+
+    if (this.result !== '') {
+      this.prevValue = this.curValue;
+      this.curValue = value;
+    }
+
+    if (value === 'C') {
+      this.result = '';
+
+    } else if (value === '=') {
+      // tslint:disable-next-line:no-eval
+      this.result = eval(this.result);
+
+    } else {
+      this.result += value;
+    }
+  }
+}
