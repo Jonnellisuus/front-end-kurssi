8288fb468c0e9ffaaef2f8821da72d07eeb14b0d
Jonne Kiukas <jonne.kiukas@hotmail.com>
2019-11-04 13:54:34 +0200

    Toolbar initialized.

--- HelloWorld/src/app/app.module.ts
+++ HelloWorld/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import {MatButtonModule, MatGridListModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
