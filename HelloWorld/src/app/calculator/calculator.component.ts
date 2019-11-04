f0576562131c9dd0c27e8ca63ece37b39a772b46
Jonne Kiukas <jonne.kiukas@hotmail.com>
2019-11-04 13:19:12 +0200

    User cannot add two operators now. Made position changes in calculator.component.css file. Calculator task done.

--- HelloWorld/src/app/calculator/calculator.component.ts
+++ HelloWorld/src/app/calculator/calculator.component.ts
@@ -10,7 +10,7 @@ export class CalculatorComponent implements OnInit {
   buttons: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '0', '+', '='];
   result: string = '';
   private prevValue: string = '';
-  private curValue: string = '';
+  private nextValue: string = '';
 
   constructor() {
   }
@@ -19,10 +19,9 @@ export class CalculatorComponent implements OnInit {
   }
 
   addValue(value: string) {
-
     if (this.result !== '') {
-      this.prevValue = this.curValue;
-      this.curValue = value;
+      this.prevValue = this.nextValue;
+      this.nextValue = value;
     }
 
     if (value === 'C') {
@@ -34,6 +33,20 @@ export class CalculatorComponent implements OnInit {
 
     } else {
       this.result += value;
+      this.prevValue = this.result[this.result.length - 1];
+      this.nextValue = this.result[this.result.length - 2];
+
+      if (this.prevValue === '/' && this.nextValue === '/' || this.prevValue === '/' && this.nextValue === '*' ||
+        this.prevValue === '/' && this.nextValue === '+' || this.prevValue === '/' && this.nextValue === '-' ||
+        this.prevValue === '*' && this.nextValue === '/' || this.prevValue === '*' && this.nextValue === '*' ||
+        this.prevValue === '*' && this.nextValue === '+' || this.prevValue === '*' && this.nextValue === '-' ||
+        this.prevValue === '+' && this.nextValue === '/' || this.prevValue === '+' && this.nextValue === '*' ||
+        this.prevValue === '+' && this.nextValue === '+' || this.prevValue === '+' && this.nextValue === '-' ||
+        this.prevValue === '-' && this.nextValue === '/' || this.prevValue === '-' && this.nextValue === '*' ||
+        this.prevValue === '-' && this.nextValue === '+' || this.prevValue === '-' && this.nextValue === '-') {
+
+        this.result = this.result.substring(0, this.result.length - 1);
+      }
     }
   }
 }
