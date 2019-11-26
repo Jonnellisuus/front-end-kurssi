f9955fd2ef5f41fe1db88aa03dd9d15bc4c71c35
Jonne Kiukas <jonne.kiukas@hotmail.com>
2019-11-26 08:52:47 +0200

    Added routing for cinema.

--- HelloWorld/src/app/app-routing.module.ts
+++ HelloWorld/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {FeedbackComponent} from './feedback/feedback.component';
 import {FormComponent} from './form/form.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {ReactiveFormComponent} from './reactive-form/reactive-form.component';
+import {CinemaComponent} from './cinema/cinema.component';
 
 
 const routes: Routes = [
@@ -12,7 +13,8 @@ const routes: Routes = [
   {path: 'calculator', component: CalculatorComponent},
   {path: 'feedback', component: FeedbackComponent},
   {path: 'form', component: FormComponent},
-  {path: 'reactive-form', component: ReactiveFormComponent},
+  {path: 'reactiveform', component: ReactiveFormComponent},
+  {path: 'cinema', component: CinemaComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 
