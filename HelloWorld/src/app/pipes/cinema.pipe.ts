1d01a41410fa66fbf543f7df44c4eb2e5d805e0d
Jonne Kiukas <jonne.kiukas@hotmail.com>
2020-02-23 19:00:49 +0200

    Made the code so the result is more accurate.

--- HelloWorld/src/app/pipes/cinema.pipe.ts
+++ HelloWorld/src/app/pipes/cinema.pipe.ts
@@ -7,9 +7,17 @@ export class CinemaPipe implements PipeTransform {
 
   transform(value: any, filter: any): any {
 
+    /*
     if (!value || !filter) {
       return value;
     }
     return value.filter(item => (JSON.stringify(item).toLowerCase().includes(filter)));
+  }
+     */
+    // This is a better way than the above one.
+    if (!value || !filter) {
+      return value;
+    }
+    return value.filter(newItem => newItem.Title.toLowerCase().includes(filter.toLowerCase()));
   }
 }
