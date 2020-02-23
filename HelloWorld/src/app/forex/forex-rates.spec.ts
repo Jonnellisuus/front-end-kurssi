d6a5be80fb482dca0f5d762f1673168c2a48aef4
Jonne Kiukas <jonne.kiukas@hotmail.com>
2020-02-23 19:03:01 +0200

    Currency exchange task done.

---
+++ HelloWorld/src/app/forex/forex-rates.spec.ts
@@ -0,0 +1,7 @@
+import { ForexRates } from './forex-rates';
+
+describe('ForexRates', () => {
+  it('should create an instance', () => {
+    expect(new ForexRates()).toBeTruthy();
+  });
+});
---
+++ HelloWorld/src/app/forex/forex.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnInit} from '@angular/core';
+import {ForexRatesService} from '../services/forex.service';
+import {ForexRates} from './forex-rates';
+
+@Component({
+  selector: 'app-forex',
+  templateUrl: './forex.component.html',
+  styleUrls: ['./forex.component.css']
+})
+export class ForexComponent implements OnInit {
+  forexList: string[];
+  forexRates: ForexRates;
+  selectedForex: string;
+  targetForex: string;
+  currentRate: number;
+  inputAmount: number;
+  targetAmount: string;
+
+  constructor(private forexRatesService: ForexRatesService) {
+    this.targetAmount = '';
+    this.currentRate = 1;
+    this.forexList = ['EUR'];
+    this.forexRates = new ForexRates();
+  }
+
+  ngOnInit() {
+    this.forexRatesService.getRatesData().subscribe(response => {
+      const ratesArray = Object.entries(response.rates);
+      ratesArray.forEach(([key]) => {
+        this.forexList.push(key);
+      });
+    });
+  }
+
+  onChange() {
+    if (this.targetForex !== undefined && this.inputAmount !== undefined && this.selectedForex !== undefined) {
+      this.forexRatesService.getData(this.selectedForex).subscribe(response => {
+        this.forexRates = response;
+        const ratesArray = Object.entries(this.forexRates.rates);
+
+        ratesArray.forEach(([key, value]) => {
+          if (key === this.targetForex) {
+            this.currentRate = value;
+          }
+        });
+        console.log('Summa = ' + this.inputAmount * this.currentRate);
+        this.targetAmount = (this.inputAmount * this.currentRate).toFixed(2);
+      });
+    }
+  }
+}
